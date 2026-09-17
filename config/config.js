00ffd734cc561afc0e28daa28c83034435b9ec4d
ephemeral <120194456+V01dNullified@users.noreply.github.com>
2026-09-17 01:28:20 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,24 +1,10 @@
 /** @type {import('../play.pokemonshowdown.com/src/client-main').PSConfig} */
 var Config = Config || {};
 
-/* version */ Config.version = "0";
+Config.version = "0";
 
-Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
-
-Config.whitelist = [
-	'wikipedia.org'
-
-	// The full list is maintained outside of this repository so changes to it
-	// don't clutter the commit log. Feel free to copy our list for your own
-	// purposes; it's here: https://play.pokemonshowdown.com/config/config.js
-
-	// If you would like to change our list, simply message Zarel on Smogon or
-	// Discord.
-];
-
-// `defaultserver` specifies the server to use when the domain name in the
-// address bar is `Config.routes.client`.
-Config.defaultserver = {
+// Explicitly define the target server object
+Config.server = {
 	id: 'showdown',
 	host: 'sim3.psim.us',
 	port: 443,
@@ -27,15 +13,10 @@ Config.defaultserver = {
 	registered: true
 };
 
-Config.roomsFirstOpenScript = function () {
-};
+Config.defaultserver = Config.server;
 
-Config.customcolors = {
-	'zarel': 'aeo'
-};
-/*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (01dfaaff/f2faebd6)";
-Config.translationCachebuster = "";
+// Enable cross-origin authentication handling for third-party domains
+Config.testclient = true;
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
